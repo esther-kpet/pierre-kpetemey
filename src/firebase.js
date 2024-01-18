cc96c4895a6cc8084bc1603c5fdd6960334fde9f
Esther Kpetemey <kpetemeyesther@gmail.com>
2024-01-18 20:54:57 +0000

    x

--- src/firebase.js
+++ src/firebase.js
@@ -5,16 +5,17 @@ import { getStorage } from 'firebase/storage';
 import { getFirestore } from 'firebase/firestore/lite';
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyASCURXcSSZmmunFlfc1DcqPWuEU25TYj8",
+  authDomain: "pierre-kpetemey.firebaseapp.com",
+  projectId: "pierre-kpetemey",
+  storageBucket: "pierre-kpetemey.appspot.com",
+  messagingSenderId: "226695211569",
+  appId: "1:226695211569:web:e4d58c64135e0d3de28310"
 };
 
 
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 export const auth = getAuth();
 const provider = new GoogleAuthProvider();
 export const db = getFirestore(app);
